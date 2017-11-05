2998da633a082ad8e14b2e452fcb3b3133acb655
sgc88 <sehribancheng@yahoo.com>
2017-11-05 00:16:18 -0700

    index.html is ready and connected to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res){
-  res.send("Hello World");
+  res.sendFile("views/index.html", {root : __dirname});
 });
 
 app.listen(3000, function(){
